640c29811147ca4bb149e5190019d0adf50df311
ajaykumar1007 <118360153+ajaykumar1007@users.noreply.github.com>
2023-04-25 18:14:54 +0530

    Create a.py

---
+++ a.py
@@ -0,0 +1,12 @@
+import git
+
+source_repo = git.Repo('../')
+latest_tag=sorted(source_repo.tags,key=lambda t: t.commit.committed_datetime)[-1]
+print(latest_tag)
+
+destination_repo=git.Repo('.')
+new_tag=destination_repo.create_tag(latest_tag)
+destination_repo.git.add(update=True)
+destination_repo.index.commit(f"Automatic build v{new_tag}")
+destination_repo.remotes.origin.push(new_tag)
+destination_repo.remotes.origin.push()
