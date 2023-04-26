787007300818956735697f65140e3b59fd648e0a
ajaykumar1007 <ajay.kumar.3@antiersolutions.com>
2023-04-26 11:41:56 +0530

    hvcshv

--- a.py
+++ a.py
@@ -9,15 +9,11 @@ except:
 print(latest_tag)
 
 destination_repo=git.Repo('.')
-new_tag=destination_repo.create_tag(latest_tag)
-# destination_repo.git.add(update=True)
-# destination_repo.index.commit(f"Automatic build v{new_tag}")
-# destination_repo.remotes.origin.push(new_tag)
-# destination_repo.remotes.origin.push()
 print("Status: ",destination_repo.git.status())
 print("Git add: ",destination_repo.git.add(['.']))
 print("git commit: ",destination_repo.index.commit(f"Automatic build v{new_tag}"))
 print("Git pushed: ",destination_repo.remotes.origin.push())
+new_tag=destination_repo.create_tag(latest_tag)
 print("Taggged: ",destination_repo.remotes.origin.push(new_tag))
 print("Active branch: ",destination_repo.active_branch.name)
 
