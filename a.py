5c10c44cb08445a683daa78f18c45186864ad120
ajaykumar1007 <ajay.kumar.3@antiersolutions.com>
2023-04-26 11:00:46 +0530

    hvscugeuduhvsdhh

--- a.py
+++ a.py
@@ -17,7 +17,7 @@ new_tag=destination_repo.create_tag(latest_tag)
 print("Status: ",destination_repo.git.status())
 print("Git add: ",destination_repo.git.add(['.']))
 print("git commit: ",destination_repo.index.commit(f"Automatic build v{new_tag}"))
+print("Git pushed: ",destination_repo.remotes.origin.push())
 print("Taggged: ",destination_repo.remotes.origin.push(new_tag))
 print("Active branch: ",destination_repo.active_branch.name)
-print("Git pushed: ",destination_repo.remotes.origin.push())
 
